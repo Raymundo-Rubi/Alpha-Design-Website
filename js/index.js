1a5c404ad1c890de5fd7f8cbdb80b3cd51de9a0e
Raymundo-Rubi <44483565+Raymundo-Rubi@users.noreply.github.com>
2019-08-28 10:53:49 -0700

    Added Slideshow JS
    need to fix responsive sizing

--- js/index.js
+++ js/index.js
@@ -9,3 +9,27 @@ $('.funFacts').delay(85).fadeIn(1500);
     });
   });
 })(jQuery);
+
+var i = 0; //start point of slideshow
+var images = [];
+var time = 2000;
+
+//image list
+images[0] = './images/sign_slide.jpg'
+images[1] = './images/work_slide.jpg'
+images[2] = './images/office_slide.jpg'
+
+//Change image
+function changeImg(){
+	document.slide.src = images[i];
+
+	if(i < images.length - 1){
+		i++;
+	} else {
+		i = 0;
+	}
+
+	setTimeout("changeImg()", time);
+}
+
+window.onload = changeImg;
